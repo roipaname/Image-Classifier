cd4517adef59e6acd1b106dc31076d9fe59b1196
roipaname <roipaname@gmail.com>
2026-01-01 21:17:32 +0200

    setting up model

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from classifier!")
+import cv2
+import numpy as np
+import streamlit as st
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+from PIL import Image
 
+def load_model():
+    model=MobileNetV2(weights='imagenet')
+    return model
 
-if __name__ == "__main__":
-    main()
+def preprocess_image(image):
+    
+    image=np.array(image)
+    image=cv2.resize(image,(244,244))
+    image=preprocess_input(image)
+    image=np.expand_dims(image,axis=0)
+    return image
